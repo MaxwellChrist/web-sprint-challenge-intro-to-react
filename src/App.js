d78f201d4e9df726a74dffe7c504c4941d92edde
MaxwellChrist <ribthulu@gmail.com>
2022-02-18 08:47:55 -0500

    got slices of state & API request running

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
-console.log("test");
-
-const App = () => {
+  const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+
+    // testing one user before creating many
+    const luke = {
+      "name": "Luke Skywalker", 
+      "height": "172", 
+      "mass": "77", 
+      "hair_color": "blond", 
+      "skin_color": "fair", 
+      "eye_color": "blue", 
+      "birth_year": "19BBY", 
+      "gender": "male", 
+      }
+
+  const [character, setCharacter] = useState(luke);
+
+
+  const [characterList, setCharacterList] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people")
+      .then(res => {
+        console.log(res.data);
+        setCharacterList(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, []);
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characterList.map(character=> {
+          return (
+            <h3>Name: {character.name}</h3>
+          )
+        })
+      }
     </div>
   );
 }
