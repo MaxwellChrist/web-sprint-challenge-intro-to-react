4dec86bd41bc9bb034415b5a4435783ac252a40b
MaxwellChrist <ribthulu@gmail.com>
2022-02-18 08:15:30 -0500

    first commit not appearing in github, second attempt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+console.log("test");
+
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
