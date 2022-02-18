7fc698abd6653ae48d9c5676a4436d0f0f674285
MaxwellChrist <ribthulu@gmail.com>
2022-02-18 10:13:01 -0500

    cleaned up code, added installed reactstrap, and edited basic CSS

--- src/App.js
+++ src/App.js
@@ -3,24 +3,7 @@ import './App.css';
 import axios from 'axios';
 import Character from './components/Character';
 
-
   const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // testing one user before creating many
-  //   const luke = {
-  //     name: "Luke Skywalker", 
-  //     height: "172", 
-  //     mass: "77", 
-  //     hair_color: "blond", 
-  //     skin_color: "fair", 
-  //     eye_color: "blue", 
-  //     birth_year: "19BBY", 
-  //     gender: "male", 
-  //     }
-
-  // const [character, setCharacter] = useState(luke);
   const [characterList, setCharacterList] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
@@ -47,20 +30,3 @@ import Character from './components/Character';
 }
 
 export default App;
-
-
-// {characterList.map(character=> {
-//   return (
-//     <div>
-//       <h3>Name: {character.name}</h3>
-//       <p>Height: {character.height}</p>
-//       <p>Mass: {character.mass}</p>
-//       <p>Hair color: {character.hair_color}</p>
-//       <p>Skin color: {character.skin_color}</p>
-//       <p>Eye color: {character.eye_color}</p>
-//       <p>Birth Year: {character.birth_year}</p>
-//       <p>Gender: {character.gender}</p>
-//     </div>
-//   )
-// })
-// }
